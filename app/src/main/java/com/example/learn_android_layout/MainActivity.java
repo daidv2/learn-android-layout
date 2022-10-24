62498fe072e9f51156b82d70d820e86e48883aa1
rk_vinhdh <vinhdh@rikkeisoft.com>
2022-10-24 18:11:28 +0700

    first commit

---
+++ app/src/main/java/com/example/learn_android_layout/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.learn_android_layout;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.image);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Collapsing");
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.colorAccent));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
